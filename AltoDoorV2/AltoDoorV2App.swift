6d061430934e07223a2bef004273fea2d30d510b
omursnck <omrsnr12@icloud.com>
2023-11-27 02:09:36 +0300

    Initial Commit

---
+++ AltoDoorV2/AltoDoorV2App.swift
@@ -0,0 +1,17 @@
+//
+//  AltoDoorV2App.swift
+//  AltoDoorV2
+//
+//  Created by Ömür Şenocak on 27.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct AltoDoorV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
